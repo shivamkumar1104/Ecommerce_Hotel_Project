6fa7a1c9636fad9d2280bd139abb1709fda1f99d
shivamkumar1104 <shivamkumaar2004@gmail.com>
2026-09-06 16:29:23 +0530

    Ecommerce_Hotel_Project

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,2 +1,16 @@
-import db from './config/db.js';
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
+const USerSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: 
+        }
+    }
+)
+const User = {
+    ["name": "String",
+
+    ]
+}
