8ef5cfab61c120101c153c36354bff6eba50fe3d
shivamkumar1104 <shivamkumaar2004@gmail.com>
2026-09-06 16:09:19 +0530

    Ecommerce_Hotel_Project

--- gitpush.bat
+++ gitpush.bat
@@ -2,7 +2,7 @@
 
 cd /d "C:\Users\hp\OneDrive\Desktop\hotel"
 echo Starting Auto-Push script for hotel E-Commerce website
-echo It will check and push your code to GitHub every 10 minutes.
+echo It will check and push your code to GitHub every 20 minutes.
 echo Keep this window open while you are coding.
 echo Press Ctrl+C to stop.
 echo.
@@ -13,5 +13,5 @@ git add .
 git commit -m "Ecommerce_Hotel_Project" >nul 2>&1
 git push origin main >nul 2>&1
 
-timeout /t 600 /nobreak >nul
+timeout /t 1200 /nobreak >nul
 goto loop
